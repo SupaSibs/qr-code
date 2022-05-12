97933a2bab6c79912547bbb918f7d85cafe819a7
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-05-12 04:45:11 +0000

    basic componentsd

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
-
+import Card from "/src/lib/Card.svelte"
+  import Attribution from "/src/lib/Attribution.svelte"
 </script>
 
+
+<Card />
+  
--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -4,9 +4,9 @@
 
 <div>
 
-  Improve your front-end skills by building projects
-
+  <h1>Improve your front-end skills by building projects</h1>
+<p>
   Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-  
+</p>
 
 </div>
