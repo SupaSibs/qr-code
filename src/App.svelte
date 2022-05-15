f910944bf7724cf207708ffac854cb32f0f3e9cf
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-05-15 04:42:46 +0000

    yyfffdtjdewras sh ert    rtjh d ey thd resa nsagh

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,15 @@ import Card from "/src/lib/Card.svelte"
   import Attribution from "/src/lib/Attribution.svelte"
 </script>
 
-
+<div>
 <Card />
-  
+</div>
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+  }
+</style>
